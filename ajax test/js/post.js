964acffd4fc7e0e11b71f4dc8e1bec52d1f4fc95
Derek Markey <derek@climbthepage.com>
2013-12-03 14:52:06 -0600

    Changes made by Derek Markey (derek@climbthepage.com).

--- ajax test/js/post.js
+++ ajax test/js/post.js
@@ -1,5 +1,5 @@
 $('#posting').click(function(){
-    $.post("http://www.lifestimecapsule.com/ajax/authenticate", { "email": "hermanjr2010@gmail.com", "password": "xxx123", crossDomain: true })
+    $.post("http://www.wiscribe.com/ajax/authenticate", { "email": "hermanjr2010@gmail.com", "password": "xxx123", crossDomain: true })
      .success(function(data) { 
                 var obj = eval('(' + data + ')');
                  alert(obj.message);
@@ -32,9 +32,29 @@ $('#posting').click(function(){
 //    });
 
 $('#testupload').click(function(){
-     $.post("http://www.lifestimecapsule.com/ajax/upload", { "title": "ipdad ajax test", "content": "askdmsfjgsdfklmdfjklmgdfg", "media": 'journal', crossDomain: true })
+     $.post("http://www.wiscribe.com/ajax/upload", { "title": "ipdad ajax test", "content": "askdmsfjgsdfklmdfjklmgdfg", "media": 'journal', crossDomain: true })
                     .success(function(data) { 
                         console.log(data);
                     })
                     .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); });
 });
+
+
+var input = document.querySelector('input[type=file]'); // see Example 4
+
+input.onchange = function () {
+  var file = input.files[0];
+
+  upload(file);
+  drawOnCanvas(file);   // see Example 6
+  displayAsImage(file); // see Example 7
+};
+
+function upload(file) {
+  var form = new FormData(),
+      xhr = new XMLHttpRequest();
+
+  form.append('video', file);
+  xhr.open('post', 'http://www.wiscribe.com/ajax/upload', true);
+  xhr.send(form);
+}
